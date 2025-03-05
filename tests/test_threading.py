e70f88d3aa0e27948e625d6e5d243348e97af953
Handzsuj, Thomas <thomas.handzsuj@draeger.com>
2025-03-05 17:18:16 +0100

    add test for multiple schedulers in multiple threads

--- tests/test_threading.py
+++ tests/test_threading.py
@@ -3,15 +3,29 @@ from threading import Event, Thread
 
 import pytest
 
-from src.ischedule import reset, run_loop, schedule
+from src.ischedule import reset, run_loop, schedule, Scheduler
 
 
 def task_1():
     print("Task 1")
+    t1 = time.monotonic()
+    while True:
+        time.sleep(0.1)  # w/o sleep GIL attacks
+        if time.monotonic() - t1 > 0.5:
+            break
+    print("Task 1 end", t1, time.monotonic())
 
 
 def task_2():
-    print("Task 2")
+    print("Task 2", time.monotonic())
+
+
+def task_3():
+    print("Task 3", time.monotonic())
+
+
+def task_4():
+    print("Task 4", time.monotonic())
 
 
 def test_thread_1():
@@ -32,8 +46,25 @@ def test_thread_2():
     stop.set()
 
 
+def test_multi_thread():
+    sch1 = Scheduler()
+    sch1.schedule(task_1, interval=0.1)
+    sch1.schedule(task_2, interval=0.2)
+    sch2 = Scheduler()
+    sch2.schedule(task_3, interval=0.3)
+    sch2.schedule(task_4, interval=0.5)
+    stop1 = Event()
+    stop2 = Event()
+    Thread(target=sch1.run_loop, kwargs={"stop_event": stop1}, daemon=False).start()
+    Thread(target=sch2.run_loop, kwargs={"stop_event": stop2}, daemon=False).start()
+
+    time.sleep(3)
+    stop1.set()
+    stop2.set()
+
+
 if __name__ == "__main__":
-    test_thread_1()
+    test_multi_thread()
 
 
 @pytest.fixture(autouse=True)
